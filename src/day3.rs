9fb042430acd2ca718c82ea11a1e946004c7b6ca
Brett Norris <b.norris@calendly.com>
2021-12-03 17:10:54 -0500

    Day 3 - Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,47 +1,103 @@
 use std::iter::{Iterator, FromIterator};
 
-pub fn compute_significant_bitmap(input: &Vec<Vec<char>>, most: bool) -> u16 {
+pub fn filter_by_common(input: &Vec<Vec<char>>, position: usize, most: bool) -> Vec<Vec<char>> {
+    let common_value = match most {
+        true => most_common_value(input, position),
+        false => least_common_value(input, position)
+    };
+    let filtered_rows = filter_by_value(input, position, common_value);
+
+    match filtered_rows.len() {
+        0 => filtered_rows,
+        1 => filtered_rows,
+        _ => filter_by_common(&filtered_rows, position + 1, most)
+    }
+}
+
+pub fn filter_by_value(input: &Vec<Vec<char>>, position: usize, value: char) -> Vec<Vec<char>> {
+   input
+       .into_iter()
+       .filter(|val| {
+           val[position] == value
+       })
+       .cloned()
+       .collect::<Vec<Vec<char>>>()
+}
+
+pub fn most_common_value(input: &Vec<Vec<char>>, position: usize) -> char {
+    let (ones, zeroes): (Vec<char>, Vec<char>) = input
+        .into_iter()
+        .map(|val| val[position] )
+        .partition(|&n| n == '1' );
+
+    match ones.len() >= zeroes.len() {
+        true => '1',
+        false => '0'
+    }
+}
+
+pub fn least_common_value(input: &Vec<Vec<char>>, position: usize) -> char {
+    let (ones, zeroes): (Vec<char>, Vec<char>) = input
+        .into_iter()
+        .map(|val| val[position] )
+        .partition(|&n| n == '1' );
+
+    match ones.len() >= zeroes.len() {
+        true => '0',
+        false => '1'
+    }
+}
+
+pub fn compute_significant_bitmap(input: &Vec<Vec<char>>, most: bool) -> u32 {
     let mut bit_string: Vec<char> = Vec::new();
     let length = input[0].len();
+
     for i in 0..length {
-        println!("{}", i);
-
-        let (ones, zeroes): (Vec<char>, Vec<char>) = input
-            .into_iter()
-            .map(|val| val[i] )
-            .partition(|&n| n == '1' );
-
-        let mut new_value = '0';
-
-        if most && ones.iter().count() > zeroes.iter().count() || !most && ones.iter().count() < zeroes.iter().count() {
-           new_value = '1';
-        }
+        let new_value = match most {
+            true => most_common_value(input, i),
+            false => least_common_value(input, i)
+        };
 
         bit_string.push(new_value)
     }
 
-    println!("{:?}", bit_string);
-    u16::from_str_radix(&String::from_iter(bit_string), 2).unwrap()
+    u32::from_str_radix(&String::from_iter(bit_string), 2).unwrap()
+}
+
+pub fn chars_to_int(input: &Vec<char>) -> u32 {
+    u32::from_str_radix(&String::from_iter(input), 2).unwrap()
 }
 
 
 #[aoc(day3, part1)]
-pub fn part1(input: &str)  -> u16 {
+pub fn part1(input: &str)  -> u32 {
     let values: Vec<Vec<char>> = input
         .lines()
         .map(|val| { val.chars().collect() })
         .collect::<Vec<Vec<char>>>();
 
-    println!("values: {:?}", values);
-
     let gamma = compute_significant_bitmap(&values, true);
     let epsilon = compute_significant_bitmap(&values, false);
-    println!("gamma: {:?}, epsilon: {:?}", gamma, epsilon);
 
-    return gamma * epsilon;
+    let product = gamma * epsilon;
+
+    product
 }
 
+#[aoc(day3, part2)]
+pub fn part2(input: &str) ->  u32 {
+    let values: Vec<Vec<char>> = input
+        .lines()
+        .map(|val| { val.chars().collect() })
+        .collect::<Vec<Vec<char>>>();
 
+    let oxygen_rating = chars_to_int(&filter_by_common(&values, 0, true)[0]);
+    let co2_rating = chars_to_int(&filter_by_common(&values, 0, false)[0]);
+
+    println!("oxygen_rating: {:?}, co2_rating: {:?}", oxygen_rating, co2_rating);
+
+    oxygen_rating * co2_rating
+}
 
 #[cfg(test)]
 mod tests {
@@ -54,5 +110,11 @@ mod tests {
         assert_eq!(part1(input), 198)
     }
 
+    #[test]
+    fn example2() {
+        let input = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n";
+
+        assert_eq!(part2(input), 230)
+    }
 
 }
